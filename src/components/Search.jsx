3402a9863b09923a3f4916897774ae90b8d7fa16
Akash <akashghode1234@gmail.com>
2022-10-01 11:07:39 +0530

    1-10-2022

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 import '../App.css'
 import Show from './Show'
@@ -6,61 +6,54 @@ import Actor from './Actor'
 
 const Search = () => {
 
-    const [option, setOption] = useState("default")
-    const [input, setInput] = useState('')
-    const [actorData, setActorData] = useState([])
-    const [showData, setShowData] = useState([])
-    // const [showActorData, setShowActorData] = useState([]);
-  
-    useEffect(() => {
-      if (option === "actor") {
-        fetch(`https://api.tvmaze.com/search/people?q=${input}`)
-          .then((result) => {
-            result.json()
+  const [option, setOption] = useState("default")
+  const [input, setInput] = useState('')
+  const [actorData, setActorData] = useState([])
+  const [showData, setShowData] = useState([])
+  const [placeholder,setPlaceholder] = useState("select a option to search");
+
+
+  useEffect(() => {
+    if (option === "actor") {
+      fetch(`https://api.tvmaze.com/search/people?q=${input}`)
+        .then((result) => {
+          result.json()
             .then((response) => {
               setActorData(response);
             });
-          }
-          );
-          // console.log(actorData)
-      } else {
-        if (input !== "") {
-          fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(
-            (result) => {
-              result.json().then((response) => {
-                setShowData(response);
-              });
-            }
-          );
-          // console.log(showData)
         }
+        );
+      // console.log(actorData)
+    } else {
+      if (input !== "") {
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(
+          (result) => {
+            result.json().then((response) => {
+              setShowData(response);
+            });
+          }
+        );
+        // console.log(showData)
       }
-    }, [input]);
+    }
+  }, [input]);
 
-    // useEffect(() => {
-    //   if (actorData.length !== 0) {
-    //     if (input === "") {
-    //       setShowActorData([]);
-    //     } else {
-    //       fetch(
-    //         `https://api.tvmaze.com/people/${actorData[0]?.person?.id}/castcredits?embed=show`
-    //       ).then((result) => {
-    //         result.json().then((response) => {
-    //           setShowActorData(response);
-    //         });
-    //       });
-    //       console.log(showActorData);
-    //     }
-    //   }
-    // }, [actorData, input]);
 
-    const optionHandler = (e) =>{
-      setOption(e.target.value)
-      setInput('')
-      setActorData([])
-      setShowData([])
-      
-    };
+  const optionHandler = (e) => {
+    setOption(e.target.value)
+    setInput('')
+    setActorData([])
+    setShowData([])
+    if(option==='show') setPlaceholder("Enter the show name e.g. Friends")
+    else if(option === 'actor') setPlaceholder("Enter the actor name e.g. Dwayne Johnson")
+    else setPlaceholder("select a option to search")
+
+  };
+  useEffect(()=>{
+    if(option==='show') setPlaceholder("e.g. Friends")
+    else if(option === 'actor') setPlaceholder("e.g. Dwayne Johnson")
+    else setPlaceholder("select a option to search")
+  },[option])
 
 
   return (
@@ -72,22 +65,22 @@ const Search = () => {
           <option value={"actor"} >Actor</option>
         </select>
 
-        <input 
-          className='search-box' 
-          type={'search'} 
-          value={input} 
-          placeholder={'eg. Alan'}
-          onChange={(e)=>{
+        <input
+          className='search-box'
+          type={'search'}
+          value={input}
+          placeholder={placeholder}
+          onChange={(e) => {
             // console.log(option)
-              option === 'show' || option=== 'actor'?
+            option === 'show' || option === 'actor' ?
               setInput(e.target.value)
               :
               alert("'Search by Show' or 'Search by Actor'")
           }}
-          />
+        />
       </div>
-      {option === "actor" && input!=="" ?  actorData.length !== 0 ? <Actor data={actorData}/> : <h3 className="noData">Results for '{input}' not found</h3> : ""}
-      {option === "show" && input!=="" ? showData.length !== 0 ? <Show data={showData}/> : <h3 className="noData">Results for '{input}' not found</h3> : ""}
+      {option === "actor" && input !== "" ? actorData.length !== 0 ? <Actor data={actorData} /> : <h3 className="noData">Results for '{input}' not found</h3> : ""}
+      {option === "show" && input !== "" ? showData.length !== 0 ? <Show data={showData} /> : <h3 className="noData">Results for '{input}' not found</h3> : ""}
     </>
   )
 }
